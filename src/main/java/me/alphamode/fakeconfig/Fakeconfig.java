cbd13bdb9753c66c49fc703399e37dfb2613b741
alpha <alphamodeyt@gmail.com>
2022-07-26 16:41:49 -0500

    Initial commit

---
+++ src/main/java/me/alphamode/fakeconfig/Fakeconfig.java
@@ -0,0 +1,10 @@
+package me.alphamode.fakeconfig;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fakeconfig implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/alphamode/fakeconfig/client/FakeconfigClient.java
@@ -0,0 +1,13 @@
+package me.alphamode.fakeconfig.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FakeconfigClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
